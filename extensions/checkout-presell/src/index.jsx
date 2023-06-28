7c45df28bee74a6a9bfbbe7d0c88dea5a3f9a2fe
Joe Keohan <jkeohan@gmail.com>
2023-06-28 11:12:59 -0400

    initial setup

---
+++ extensions/checkout-presell/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  useExtensionApi,
+  render,
+  Banner,
+  useTranslate,
+} from '@shopify/checkout-ui-extensions-react';
+
+render('Checkout::Dynamic::Render', () => <App />);
+
+function App() {
+  const {extensionPoint} = useExtensionApi();
+  const translate = useTranslate();
+  return (
+    <Banner title="checkout-presell">
+      {/* {translate('welcome', {extensionPoint})} */}
+      Hello World
+    </Banner>
+  );
+}
